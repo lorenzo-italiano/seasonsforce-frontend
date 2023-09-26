2929fd454a1d39dbebe8efe92591988ede384f35
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2023-09-26 21:51:51 +0200

    refactor(queries): generalised getAll request

--- src/rest/hook/useFetchRecruiterList.tsx
+++ src/rest/hook/useFetchRecruiterList.tsx
@@ -1,11 +1,11 @@
 import {useQuery} from "@tanstack/react-query";
-import getAllRecruiters from "../queries/RecruiterQueries";
+import {getAll} from "../queries/RecruiterQueries";
 
 export default function useFetchRecruiterList() {
     return useQuery({
         queryKey: ["recruiter-list"],
         queryFn: async (): Promise<number> => {
-            return await getAllRecruiters();
+            return await getAll("http://localhost:8080/api/v1/recruiter/");
         }
     })
 
