4b6a4288e2aeee9c72ab502892eb401b8ac5f8be
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2023-09-28 10:57:07 +0200

    refactor(screens): Added an alias @screen for screen path.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,17 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"]
-  };
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            "nativewind/babel",
+            [
+                "module-resolver",
+                {
+                    alias: {
+                        "@screen": "./src/screen"
+                }
+            }
+        ]
+    ]
+    };
 };
