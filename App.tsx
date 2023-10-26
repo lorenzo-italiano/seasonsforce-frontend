4e2d350cba3df4e1d3c0763d67cc242dabf4673a
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2023-10-26 20:50:37 +0200

    refactor(navigation): Changed navigation system to meet new requirements.

--- App.tsx
+++ App.tsx
@@ -13,7 +13,10 @@ import Home from "@screen/Home";
 import {useEffect, useState} from "react";
 import Login from "./src/page/Login";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {NavigationProvider} from "./src/NavigationProvider";
+// import {NavigationProvider} from "./src/NavigationProvider";
+import {createStackNavigator} from "@react-navigation/stack";
+import TabNavigator from "./src/TabNavigator";
+import Register from "./src/page/Register";
 
 NativeWindStyleSheet.setOutput({
     default: "native",
@@ -23,8 +26,9 @@ const queryClient = new QueryClient()
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
+const Stack = createStackNavigator();
 
+export default function App() {
 	const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
 
 	useEffect(() => {
@@ -38,11 +42,9 @@ export default function App() {
 		checkAuthentication();
 	}, []);
 
-	// Fonction pour gérer la connexion de l'utilisateur et mettre à jour l'état d'authentification
 	const handleLogin = async () => {
-		// Effectuez la connexion, par exemple en appelant une API
 		try {
-			// Remplacez ce code par la logique de connexion réelle
+			// Effectuez la connexion, par exemple en appelant une API
 			// Si la connexion réussit, mettez à jour l'état d'authentification
 			setUserIsAuthenticated(true);
 		} catch (error) {
@@ -52,13 +54,13 @@ export default function App() {
 
 	const handleLogout = async () => {
 		try {
-			setUserIsAuthenticated(false)
+			setUserIsAuthenticated(false);
 		} catch (error) {
-			console.error('Erreur lors de la deconnexion : ', error)
+			console.error('Erreur lors de la deconnexion : ', error);
 		}
 	}
 
-    return (
+	return (
 		<QueryClientProvider client={queryClient}>
 			<NavigationContainer>
 				{userIsAuthenticated ? (
@@ -66,7 +68,9 @@ export default function App() {
 					screenOptions={{
 						//TODO mettre une couleur de la palette
 						tabBarActiveTintColor: '#a83232',
+						headerShown: false
 					}}
+					initialRouteName="Home"
 				>
 					<Tab.Screen
 						name="Home"
@@ -116,9 +120,113 @@ export default function App() {
 					/>
 				</Tab.Navigator>
 				) : (
-					<Login onLogin={handleLogin} />
+					<Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+						<Stack.Screen name="Login" children={() => <Login onLogin={handleLogin}/>} />
+						<Stack.Screen name="Register" component={Register} />
+					</Stack.Navigator>
 				)}
 			</NavigationContainer>
 		</QueryClientProvider>
-    );
+	);
 }
+
+
+// export default function App() {
+//
+// 	const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
+//
+// 	useEffect(() => {
+// 		const checkAuthentication = async () => {
+// 			const token = await AsyncStorage.getItem('authToken');
+// 			if (token) {
+// 				setUserIsAuthenticated(true);
+// 			}
+// 		};
+//
+// 		checkAuthentication();
+// 	}, []);
+//
+// 	// Fonction pour gérer la connexion de l'utilisateur et mettre à jour l'état d'authentification
+// 	const handleLogin = async () => {
+// 		// Effectuez la connexion, par exemple en appelant une API
+// 		try {
+// 			// Remplacez ce code par la logique de connexion réelle
+// 			// Si la connexion réussit, mettez à jour l'état d'authentification
+// 			setUserIsAuthenticated(true);
+// 		} catch (error) {
+// 			console.error('Erreur lors de la connexion : ', error);
+// 		}
+// 	};
+//
+// 	const handleLogout = async () => {
+// 		try {
+// 			setUserIsAuthenticated(false)
+// 		} catch (error) {
+// 			console.error('Erreur lors de la deconnexion : ', error)
+// 		}
+// 	}
+//
+//     return (
+// 		<QueryClientProvider client={queryClient}>
+// 			<NavigationContainer>
+// 				{userIsAuthenticated ? (
+// 					<Tab.Navigator
+// 					screenOptions={{
+// 						//TODO mettre une couleur de la palette
+// 						tabBarActiveTintColor: '#a83232',
+// 					}}
+// 				>
+// 					<Tab.Screen
+// 						name="Home"
+// 						component={Home}
+// 						options={{
+// 							tabBarIcon: ({ color, size }) => (
+// 								<MaterialCommunityIcons name="home" color={color} size={size} />
+// 							),
+// 						}}
+// 					/>
+// 					<Tab.Screen
+// 						name="Messages"
+// 						component={Conversation}
+// 						options={{
+// 							tabBarIcon: ({ color, size }) => (
+// 								<MaterialCommunityIcons name="message" color={color} size={size} />
+// 							),
+// 						}}
+// 					/>
+// 					<Tab.Screen
+// 						name="Jobs"
+// 						component={Jobs}
+// 						options={{
+// 							tabBarIcon: ({ color, size }) => (
+// 								<MaterialIcons name="work" color={color} size={size} />
+// 							),
+// 						}}
+// 					/>
+// 					<Tab.Screen
+// 						name="Notifications"
+// 						component={Notification}
+// 						options={{
+// 							tabBarIcon: ({ color, size }) => (
+// 								<MaterialCommunityIcons name="bell" color={color} size={size} />
+// 							),
+// 						}}
+// 					/>
+// 					<Tab.Screen
+// 						name="Profile"
+// 						children={() => <Profile onLogout={handleLogout}/>}
+// 						// initialParams={{ onLogout: handleLogout }}
+// 						options={{
+// 							tabBarIcon: ({ color, size }) => (
+// 								<FontAwesome name="user" color={color} size={size} />
+// 							),
+// 						}}
+// 					/>
+// 				</Tab.Navigator>
+// 				) : (
+// 					<Login onLogin={handleLogin} />
+// 				)}
+// 			</NavigationContainer>
+// 		</QueryClientProvider>
+//     );
+// }
