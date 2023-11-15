32e93d3c683e8115f6a547a4b28348ae2df92475
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2023-11-15 17:30:14 +0100

    chore(pages): Moved pages from screen folder to page folder.

---
+++ src/component/list/offer/CandidateOfferList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {FlatList} from "react-native";
+
+const CandidateOfferList = (props) => {
+
+	return (
+		<FlatList data={} renderItem={} />
+	)
+
+}
+
+export default CandidateOfferList;
