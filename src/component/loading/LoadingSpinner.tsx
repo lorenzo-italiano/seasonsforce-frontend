9a98173f72af2f9bf12b4d89f0585759e6d43693
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2023-11-28 13:31:50 +0100

    refactor(LoadingSpinner): Changed color of loading spinner to match theme.

--- src/component/loading/LoadingSpinner.tsx
+++ src/component/loading/LoadingSpinner.tsx
@@ -4,7 +4,7 @@ import { View, ActivityIndicator, StyleSheet } from 'react-native';
 const LoadingSpinner = () => {
 	return (
 		<View className="flex flex-1 items-center justify-center">
-			<ActivityIndicator size="large" color="#0000ff" />
+			<ActivityIndicator size="large" color="#0D3B66" />
 		</View>
 	)
 }
